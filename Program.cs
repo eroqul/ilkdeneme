a3fbd77294f2b5a5d1d8262935b1c4cff2ef7e42
eroqul <96750363+eroqul@users.noreply.github.com>
2021-12-28 00:44:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ilk_denemem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string durum;
+            int yas;
+            Console.WriteLine("Merhaba");
+
+            Console.WriteLine("Adınızı Gir Lan");
+
+           string ad = Console.ReadLine();
+
+            Console.WriteLine("Soyadınızı Gir Lan");
+            string soyad = Console.ReadLine();
+
+            Console.WriteLine("SELAM " + ad + " " + soyad);
+
+            Console.WriteLine("nasılsın");
+
+            durum = Console.ReadLine();
+
+            Console.WriteLine("yaşın");
+
+            yas = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadLine();
+        }
+    }
+}
